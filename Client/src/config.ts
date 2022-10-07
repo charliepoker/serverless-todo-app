5660d3d184d939dd1b9604de821a36680c78a51a
charliepoker <iheanachocharlie@gmail.com>
2022-10-07 01:13:59 +0100

    final code

---
+++ Client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '1639jxslh5'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/test`
+
+export const authConfig = {
+  domain: 'dev-f8mroul0.us.auth0.com',
+  clientId: 'kqffz1VOETkImdogTZd3woFat3cj2Dd8',
+  callbackUrl: 'http://localhost:3000/callback'
+}
